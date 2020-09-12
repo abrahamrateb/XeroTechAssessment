310766465e2d943e58872ceb45d663a4bce54a18
abrahamrateb <abrahamrateb@gmail.com>
2020-09-12 17:53:14 +1200

    Initiating the WebDriver and Navigating to the Xero Login Page
    Related Work Items: #15

---
+++ XeroTechAssessment/XeroTechAssessment/Hooks/XeroHooks.cs
@@ -0,0 +1,37 @@
+﻿using BoDi;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TechTalk.SpecFlow;
+
+namespace XeroTechAssessment.Hooks
+{
+    [Binding]
+    public sealed class XeroHooks
+    {
+        // For additional details on SpecFlow hooks see http://go.specflow.org/doc-hooks
+        private IWebDriver _driver;
+        private readonly IObjectContainer _objectContainer;
+        
+        public XeroHooks(IObjectContainer objectContainer)
+        {
+            _objectContainer = objectContainer;
+        }
+
+        [BeforeScenario]
+        public void BeforeScenario()
+        {
+            _driver = new ChromeDriver();
+            _objectContainer.RegisterInstanceAs<IWebDriver>(_driver);
+        }
+
+        [AfterScenario]
+        public void AfterScenario()
+        {
+            //TODO: implement logic that has to run after executing each scenario
+        }
+    }
+}
--- XeroTechAssessment/XeroTechAssessment/StepDefinitions/XeroPortalStepDefinitions.cs
+++ XeroTechAssessment/XeroTechAssessment/StepDefinitions/XeroPortalStepDefinitions.cs
@@ -1,4 +1,5 @@
 ﻿using NUnit.Framework;
+using OpenQA.Selenium;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,16 +14,20 @@ namespace XeroTechAssessment.StepDefinitions
         // For additional details on SpecFlow step definitions see https://go.specflow.org/doc-stepdef
 
         private readonly ScenarioContext _scenarioContext;
+        private IWebDriver _driver;
 
-        public XeroPortalStepDefinitions(ScenarioContext scenarioContext)
+
+
+        public XeroPortalStepDefinitions(ScenarioContext scenarioContext, IWebDriver driver)
         {
             _scenarioContext = scenarioContext;
+            _driver = driver;
         }
 
         [Given(@"I logon to Xero")]
         public void GivenILogonToXero()
         {
-            Console.WriteLine("I logon to Xero");
+            _driver.Navigate().GoToUrl("https://login.xero.com/identity/user/login/");
         }
 
         [Then(@"I am happy")]
