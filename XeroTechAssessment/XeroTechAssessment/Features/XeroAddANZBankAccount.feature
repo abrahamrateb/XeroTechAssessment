1d23151a9781233617164a2579e35a2bfa802144
abrahamrateb <abrahamrateb@gmail.com>
2020-09-12 16:45:35 +1200

    Create  simple feature file to test pipeline
    Related Work Items: #13, #14

---
+++ XeroTechAssessment/XeroTechAssessment/Features/XeroAddANZBankAccount.feature
@@ -0,0 +1,8 @@
+﻿Feature: XeroAddANZBankAccount
+As a Xero User,
+In order to manage my business successfully,
+I want to be able to add an “ANZ (NZ)” bank account inside any Xero Organisation.
+
+Scenario: Add an ANZ Bank Account
+	Given I logon to Xero
+	Then I am happy
---
+++ XeroTechAssessment/XeroTechAssessment/StepDefinitions/XeroPortalStepDefinitions.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TechTalk.SpecFlow;
+
+namespace XeroTechAssessment.StepDefinitions
+{
+    [Binding]
+    public sealed class XeroPortalStepDefinitions
+    {
+        // For additional details on SpecFlow step definitions see https://go.specflow.org/doc-stepdef
+
+        private readonly ScenarioContext _scenarioContext;
+
+        public XeroPortalStepDefinitions(ScenarioContext scenarioContext)
+        {
+            _scenarioContext = scenarioContext;
+        }
+
+        [Given(@"I logon to Xero")]
+        public void GivenILogonToXero()
+        {
+            Console.WriteLine("I logon to Xero");
+        }
+
+        [Then(@"I am happy")]
+        public void ThenIAmHappy()
+        {
+            Assert.IsTrue(true);
+        }
+
+    }
+}
